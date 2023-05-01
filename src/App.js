6d171d39f2cc10f9ddd779d933071afa6f82e858
MunirVora <munirvora1@gmail.com>
2023-05-01 11:29:07 +0530

    completed hero and logobar section

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Modal from "./components/Modal/Modal";
 import "./styles/style.css";
+import HeroSection from "./components/HeroSection/HeroSection";
+import LogoBarSection from "./components/LogoBarSection/LogoBarSection";
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
-    if(openModal) {
+    if (openModal) {
       document.body.style.overflow = "hidden";
-    }
-    else {
+    } else {
       document.body.style.overflow = "auto";
     }
-  },[openModal]);
+  }, [openModal]);
 
   return (
     <div className="App">
-      <Header  setOpenModal={setOpenModal}/>
-      {openModal && <Modal setOpenModal={setOpenModal}/>}
+      <Header setOpenModal={setOpenModal} />
+      {openModal && <Modal setOpenModal={setOpenModal} />}
+      <HeroSection />
+      <LogoBarSection />
     </div>
   );
 }
